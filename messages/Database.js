5c8d7b440763b2a5505ef64ea392586c0a6440b3
mani1236 <puppala.mani@gmail.com>
2017-12-26 08:38:13 +0000

    changed the status description
    Added the status description

--- messages/Database.js
+++ messages/Database.js
@@ -8,16 +8,8 @@ awb.push(
     EmailId: "eva@roche.ch",
     Company: "Roche Diagnostics",
     Mobile :"+417676767676",
-    shipmentready:'18.11.2017 01:45',    
-    shipmentdeparted:'18.11.2017 07:45',
-    flightdeparture:'18.11.2017 08:45',
-    FlightArrival :'18.11.2017 09:45',
-    ShipmentArrived :'18.11.2017 10:10',   
-    ShipmentReadyForPUp :'18.11.2017 10:45',
-    ShipmentDelivered  :'Yet to deliver',
-    Itemcount :'1 Pieces',
-    value: "Shipment Arrived"
-
+    Description : "Your shipment 74312345678, with 6 pieces and 55 kilos, is booked on flight LX123 departing on 3.02.2017 from MAD to JFK. Thank you for choosing Swiss WoldCargo.",
+    value: "Shipment Created"
 },
 {
     key:   "64312345678",
@@ -59,14 +51,8 @@ awb.push(
     EmailId: "eva@roche.ch",
     Company: "Roche Diagnostics",
     Mobile :"7676767676",
-    shipmentdeparted:'18.11.2017 07:45',
-    flightdeparture:'18.11.2017 08:45',
-    FlightArrival :'18.11.2017 09:45',
-    ShipmentArrived :'18.11.2017 10:10',   
-    ShipmentReadyForPUp :'18.11.2017 10:45',
-    ShipmentDelivered  :'Yet to deliver',
-    Itemcount :'1 Pieces',
-    value: "Shipment Arrived"
+    Description: "Your shipment 44312345678 is ready for carriage at departure  airport MAD. It will be loaded on flight LX123 to ZRH on 03.02.2017 at 15:00.",
+    value: "Shipment ready for carriage"
 },
 {
     key:   "34312345678",
@@ -138,11 +124,8 @@ awb.push(
     EmailId: "eva@roche.ch",
     Company: "Roche Diagnostics",
     Mobile :"7676767678",
-    shipmentready:'18/11/2017',
-    shipmentdeparted:'18/11/2017',
-    flightdeparture:'18/11/2017',
-    Itemcount :'1',
-    value: "Shipment Arrived"
+    Description : "Your shipment 72312345678 has been loaded on flight LX123 departing from MAD on 03.02.2017 at 9:05.Flight is planned to arrive at 10:50 on 03.02.2017 in ZRH.",
+    value: "Shipment departed"
 },
 {
     key:   "71312345678",
@@ -151,11 +134,8 @@ awb.push(
     EmailId: "eva@roche.ch",
     Company: "Roche Diagnostics",
     Mobile :"7676767678",
-    shipmentdeparted:'18/11/2017',
-    flightdeparture:'18/11/2017',
-    EmailId: "tes2t@gmail.com",
-    Itemcount :'1',
-    value: "Shipment Arrived"
+    Description: "Your shipment 71312345678 has been loaded on flight LX123 departing from MAD on 03.02.2017 at 9:05.Flight LX123 has departed  from MAD on 03.02.2017 at 9:05.Flight is planned to arrive at 10:50 on 03.02.2017 in ZRH.",
+    value: "Aircraft movement outbound"
 },
 {
     key:   "74212345678",
@@ -164,11 +144,8 @@ awb.push(
     EmailId: "eva@roche.ch",
     Company: "Roche Diagnostics",
     Mobile :"7676767678",
-    shipmentdeparted:'18/11/2017',
-    flightdeparture:'18/11/2017',
-    
-    Itemcount :'1',
-    value: "Shipment Sent"
+    Description : " Your shipment 74212345678 has been loaded on flight LX123 departing from MAD on 03.02.2017 at 9:05. Flight LX123 has departed  from MAD on 03.02.2017 at 9:05. Flight LX123 has arrived in ZRH on  03.02.2017 at 9:05. ",
+    value: "Aircraft movement inbound (multi leg)"
 },
 {
     key:   "74112345678",
@@ -177,11 +154,8 @@ awb.push(
     EmailId: "eva@roche.ch",
     Company: "Roche Diagnostics",
     Mobile :"7676767678",
-    shipmentdeparted:'18/11/2017',
-    EmailId: "tes2t@gmail.com",
-    flightdeparture:'18/11/2017',
-    Itemcount :'1',
-    value: "Shipment Arrived"
+    Description : "Your shipment 74112345678 has been unloaded in ZRH from flight LX123 on 03.02.2017 at 9:55. It will continue its journey toward JNB on flight LX123, departure planned on 10.02.2017 at 15:30.",
+    value: "Freight received at destination (multi leg)"
 },
 {
     key:   "74312345668",
@@ -190,11 +164,8 @@ awb.push(
     EmailId: "eva@roche.ch",
     Company: "Roche Diagnostics",
     Mobile :"7676767678",
-    shipmentdeparted:'18/11/2017',
-    flightdeparture:'18/11/2017',
-    EmailId: "tes2t@gmail.com",
-    Itemcount :'1',
-    value: "Shipment Arriving"
+    Description: "Flight LX123 has arrived in ZRH on  03.02.2017 at 9:05.",
+    value: "Aircraft movement inbound"
 },
 {
     key:   "74312345658",
@@ -204,9 +175,41 @@ awb.push(
     EmailId: "eva@roche.ch",
     Company: "Roche Diagnostics",
     Mobile :"7676767678",
-    flightdeparture:'18/11/2017',
-    Itemcount :'1',
-    value: "Shipment Sent"
+    Description : "Your shipment 74312345658 has been unloaded in ZRH from flight LX123 on 03.02.2017 at 9:55. Itbe soon ready for pick up.",
+    value: "Freight received at destination"
+},
+{
+    key:   "74312333658",
+    shipmentready:'18/11/2017',
+    shipmentdeparted:'18/11/2017',
+    Name: "Eva Goldschmidt",
+    EmailId: "eva@roche.ch",
+    Company: "Roche Diagnostics",
+    Mobile :"7676767678",
+    Description : "Your shipment 74312333658 has been picked up and handed overs to you in JNB at 16.45 on 10.02.2017.",
+    value: "Physical shipment delivery"
+},
+{
+    key:   "74312399658",
+    shipmentready:'18/11/2017',
+    shipmentdeparted:'18/11/2017',
+    Name: "Eva Goldschmidt",
+    EmailId: "eva@roche.ch",
+    Company: "Roche Diagnostics",
+    Mobile :"7676767678",
+    Description : "We are really sorry but your shipement 74312399658 has experienced an irregularity in MAD: the shipment has not been loaded as initially booked, due to non delivery by agent.  For more details, please visit our Track and trace tool or contact your local office. ",
+    value: "Irreg. With reason code"
+},
+{
+    key:   "74312300658",
+    shipmentready:'18/11/2017',
+    shipmentdeparted:'18/11/2017',
+    Name: "Eva Goldschmidt",
+    EmailId: "eva@roche.ch",
+    Company: "Roche Diagnostics",
+    Mobile :"7676767678",
+    Description : "We are really sorry but your shipement 74312300658 has experienced an irregularity in MAD. Please visit our Track and trace tool or contact your local office. ",
+    value: "Irreg. With no reason code"
 },
 {
     key:   "74323456789",
@@ -216,9 +219,8 @@ awb.push(
     EmailId: "eva@roche.ch",
     Company: "Roche Diagnostics",
     Mobile :"7676767678",
-    flightdeparture:'18/11/2017',
-    Itemcount :'1',
-    value: "Shipment Sent"
+    Description : "Your shipment 74323456789 is ready for pick up at EWR. It can be collected within opening hours.",
+    value: "Notification to customer"
 });   
 
 var flight = []; // create an empty array
